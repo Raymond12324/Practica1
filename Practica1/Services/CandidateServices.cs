dd24edb87e6da90b6b0d886e25270b46701b0ad1
Randolph <rcarmona@simetricaconsulting.com>
2021-06-18 11:18:21 -0400

    Agregar archivos de proyecto.

---
+++ Practica1/Services/CandidateServices.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica1.Services
+{
+    public class CandidateServices : ICandidateServices
+    {
+    }
+}
